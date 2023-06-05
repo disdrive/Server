8bbed669f81163cd3b13a63d2bae9f9b9eb6a14b
RUNFUNRUN <ryotauchiyama1212@gmail.com>
2023-06-05 13:09:51 +0900

    registerの大まかな構造作成

--- src/discord/commands/register.ts
+++ src/discord/commands/register.ts
@@ -1,11 +1,54 @@
-
 import { SlashCommandBuilder } from 'discord.js';
 import type { ChatInputCommandInteraction } from 'discord.js';
 import type { command } from '../type';
 
 export const register: command = {
-  data: new SlashCommandBuilder().setName('register').setDescription('register your account'),
+  data: new SlashCommandBuilder().setName('register').setDescription('register your account')
+    .addStringOption(option => option.setName('userid').setDescription('Use at least 6 characters.').setRequired(true))
+    .addStringOption(option => option.setName('password').setDescription('Use at least 6 characters, including both numbers and letters').setRequired(true)),
   execute: async (interaction: ChatInputCommandInteraction) => {
-    await interaction.reply('pong!');
-  },
+    const userId = interaction.options.getString('userid');
+    const password = interaction.options.getString('password');
+    if (!userId || !password) {
+      // await interaction.reply('Please input userid and password.');
+      return;
+    }
+
+    // error handling
+    let error = false;
+    let errorMessages = '';
+    if (userId.length < 6) {
+      error = true;
+      errorMessages += 'Please make your user ID at least 6 characters long.\n';
+    }
+    // if (usedUserId(userId)) {
+    //   error = true;
+    //   errorMessages += 'Please use a different user ID. That one is already taken.\n';
+    // }
+    if (password.length < 6) {
+      error = true;
+      errorMessages += 'Please make your password at least 6 characters long.\n';
+    }
+    if (!/\d/.test(password)) {
+      error = true;
+      errorMessages += 'Please include at least one number in your password.\n';
+    }
+    if (!/[a-zA-Z]/.test(password)) {
+      error = true;
+      errorMessages += 'Please include at least one letter in your password.\n';
+    }
+    if (error) {
+      await interaction.reply(errorMessages);
+      return;
+    }
+
+    try {
+      // await registerUser(userId, password);
+      await interaction.reply('Your account has been registered.');
+      return;
+    } catch (error) {
+      await interaction.reply('Sorry, there was an error trying to register your account. Please try again later.\nerror: \n```\n' + error + '\n```');
+      return;
+    }
+  }
 }
