e0938795fed6a6855d6ddc121651c1d1ba077410
RUNFUNRUN <ryotauchiyama1212@gmail.com>
2023-06-06 18:26:27 +0900

    add quit

--- src/discord/commands/index.ts
+++ src/discord/commands/index.ts
@@ -1,6 +1,10 @@
 import type { command } from '../type';
 import { test } from './test';
+import { register } from './register';
+import { quit } from './quit';
 
 export const commands: command[] = [
-  test
+  test,
+  register,
+  quit
 ];
---
+++ src/discord/commands/quit.ts
@@ -0,0 +1,30 @@
+import { SlashCommandBuilder } from 'discord.js';
+import type { ChatInputCommandInteraction } from 'discord.js';
+import type { command } from '../type';
+
+export const quit: command = {
+  data: new SlashCommandBuilder().setName('quit').setDescription('quit your account')
+    .addStringOption(option => option.setName('userid').setRequired(true))
+    .addStringOption(option => option.setName('password').setRequired(true)),
+  execute: async (interaction: ChatInputCommandInteraction) => {
+    const userId = interaction.options.getString('userid');
+    const password = interaction.options.getString('password');
+    if (!userId || !password) {
+      // await interaction.reply('Please input userid and password.');
+      return;
+    }
+
+    // if (confirmUser(userId, password)) {
+    //   await interaction.reply('User id or password is incorrect.');
+    // }
+
+    try {
+      // await deleteUser(userId, password);
+      await interaction.reply('Your account has been deleted.');
+      return;
+    } catch (error) {
+      await interaction.reply('Sorry, there was an error trying to delete your account. Please try again later.\nerror: \n```\n' + error + '\n```');
+      return;
+    }
+  }
+}
