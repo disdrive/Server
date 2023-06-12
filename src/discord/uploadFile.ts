c131c4134b31dfbb71c892896be8ceb54f518cda
RUNFUNRUN <ryotauchiyama1212@gmail.com>
2023-06-12 20:08:41 +0900

    エラーハンドリング変更

--- src/discord/uploadFile.ts
+++ src/discord/uploadFile.ts
@@ -4,7 +4,7 @@ import { client } from "./app";
 export const uploadFile = async (channelId: string, filePath: string) => {
   const attachment = new AttachmentBuilder(filePath);
   const channel = client.channels.cache.get(channelId);
-  if (channel === undefined) return false;
-  if (!channel.isTextBased()) return false;
+  if (channel === undefined) throw new Error("Channel not found");
+  if (!channel.isTextBased()) throw new Error("Channel is not text based");
   await channel.send({ files: [attachment] });
 };
