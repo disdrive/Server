70e9b32c8536c79f8a5903bd151742e988c35db0
RUNFUNRUN-ubuntu <ubuntu@ip-172-31-11-74.ap-northeast-1.compute.internal>
2023-07-20 09:21:56 +0000

    レスポンス共通化

--- src/routes/v1/auth/login.ts
+++ src/routes/v1/auth/login.ts
@@ -6,16 +6,18 @@ import { makeToken } from '@/utils/makeToken';
 export const login = express.Router();
 
 login.post('/', async (req: Request, res: Response) => {
+  console.log('Received request:', req.method, req.url);
+  console.log('Request body:', req.body);
   const { userId, password } = req.body;
   if (!(await confirmUser(userId, password))) {
-    res.json({ success: false, message: 'Invalid credentials' });
+    res.json({ success: false, token: "" });
     return;
   }
   const user = await getUserInfo(userId);
   if (user && await comparePasswords(password, user.password)) {
     const token = makeToken(user.id, user.userId);
-    res.json({ success: true, token });
+    res.json({ success: true, token: token });
   } else {
-    res.json({ success: false, message: 'Invalid credentials' });
+    res.json({ success: false, token: "" });
   }
 });
