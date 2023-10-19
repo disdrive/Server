1f4cf75a674a0eebf741e4e9b278a51849794669
Buntin-Rhea <mail@buntin.xyz>
2023-10-20 03:08:34 +0900

    change header

--- src/routes/v1/test.ts
+++ src/routes/v1/test.ts
@@ -6,5 +6,6 @@ export const testRoutes = express.Router();
 // Test route
 testRoutes.get("/test", (req: Request, res: Response) => {
   Logger.getInstance().log(200, req);
+  res.header("Content-Type", "application/text");
   res.send("API is working!");
 });
