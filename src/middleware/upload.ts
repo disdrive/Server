ee1f836b38ee59aeb9d42666d65948953a49c844
RUNFUNRUN <ryotauchiyama1212@gmail.com>
2023-10-20 16:36:29 +0900

    fix cache dir

--- src/middleware/upload.ts
+++ src/middleware/upload.ts
@@ -2,7 +2,7 @@ import multer from "multer";
 
 const storage = multer.diskStorage({
   destination: (_req, _file, cb) => {
-    cb(null, "uploads/");
+    cb(null, ".cache/uploads/");
   },
   filename: (_req, file, cb) => {
     cb(null, `${Date.now()}-${file.originalname}`);
